3c69f165572751b82bf247281cd8a1bc3f3b4431
jonatas-salles <jonatas.salles@outlook.com>
2024-02-12 16:05:41 -0300

    refactor: change file name

--- CrudPractice/todolist/todolist/src/main/java/com/salles/todolist/repository/TodoRepository.java
+++ CrudPractice/todolist/todolist/src/main/java/com/salles/todolist/repository/TodoRepository.java
@@ -3,5 +3,5 @@ package com.salles.todolist.repository;
 import com.salles.todolist.entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface todoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long> {
 }
